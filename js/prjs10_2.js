a4a56c5529d32011e42a4bc5c9b370d2705f959e
Mike <mblack101@ufl.edu>
2018-11-23 20:21:51 -0500

    completed part 1

---
+++ js/prjs10_2.js
@@ -0,0 +1,7 @@
+var int = document.getElementById('int');
+int.addEventListener('mouseover', function() {
+    int.style.color = '#ff0000';
+}, false);
+int.addEventListener('mouseout', function() {
+    int.style.color = '#000000';
+}, false);
