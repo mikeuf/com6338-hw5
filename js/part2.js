df988ca2289128dce14f33825cfa7bae6007208c
Mike <mblack101@ufl.edu>
2018-11-24 02:21:58 -0500

    got mouseleave working

---
+++ js/part2.js
@@ -0,0 +1,12 @@
+/*var int = document.getElementById('int');
+int.addEventListener('mouseover', function() {
+    int.style.color = '#ff0000';
+}, false);
+int.addEventListener('mouseout', function() {
+    int.style.color = '#000000';
+}, false); */
+
+document.addEventListener('mouseleave', function() { 
+    alert('hello'); 
+});
+
